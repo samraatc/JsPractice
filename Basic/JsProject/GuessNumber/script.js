d5fa1ddfa2385f3d4ea4592220215a6461da5c4b
Bishnu001 <97343961+samraatc@users.noreply.github.com>
2024-10-21 17:42:09 +0000

    added JavaScript function

--- Basic/JsProject/GuessNumber/script.js
+++ Basic/JsProject/GuessNumber/script.js
@@ -3,3 +3,46 @@ body.style.backgroundColor = '#212121';
 body.style.color = '#ffffff';
 
 
+const randomNumber = parseInt(Math.random() * 100 + 1);
+// console.log(randomNumber);    // it test the random number
+
+const submit = document.querySelector('#submit');
+const userguess = document.querySelector('#guessFiels');
+const guessSlot = document.querySelector('.PreviousGuess');  // it hold all the Previous Guesses
+const guessCount = document.querySelector('.guessCount');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resulDisplay');
+
+const p = document.createElement('p');
+
+let guessCountNumber = 1;
+let previousGuess = [];
+
+let playGame = true;
+
+function validateUserGuess() {
+    //
+};
+
+function checkUserGuess() {
+    //
+};
+
+
+function displayGuesses(){
+    //
+};
+
+function displayMessage(){
+    //
+};
+
+function newGame(){
+    //
+};
+
+function endGame(){
+    //
+};
+
+
