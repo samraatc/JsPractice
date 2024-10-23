5e2b5e26cabeed50eadc1b0e60c9916d5c758cff
Bishnu001 <97343961+samraatc@users.noreply.github.com>
2024-10-23 10:36:25 +0000

    project Guess a Number update

--- Basic/JsProject/GuessNumber/script.js
+++ Basic/JsProject/GuessNumber/script.js
@@ -1,9 +1,10 @@
-const body = document.querySelector('body');
+   const body = document.querySelector('body');
 body.style.backgroundColor = '#212121';
 body.style.color = '#ffffff';
 
 
-const randomNumber = parseInt(Math.random() * 100 + 1);
+
+let randomNumber = parseInt(Math.random() * 100 + 1);
 // console.log(randomNumber);    // it test the random number
 
 const submit = document.querySelector('#submit');
@@ -37,29 +38,30 @@ function validateUserGuess(userinput) {
         alert('Please enter a number between 1 to 100');
     }else {
         previousGuess.push(userinput);
-        if (guessCountNumber === 11){
+        // console.log(previousGuess);    // check if user input is pushed in the array or not
+        if (guessCountNumber === 10){
             displayGuesses(userinput);
             displayMessage(`Game Over! You Lost! The Number was ${randomNumber}`);
             endGame();
         }else {
             displayGuesses(userinput);
-            checkUserGuess(userinput);
+            checkUserGuessuser(userinput);
             
         }
         // 
     }
 };
 
-function checkUserGuess() {
+function checkUserGuessuser(userinput) {
     // checkUserGuess is used to check if user input is right or wrong or equel to random number
     if (userinput === randomNumber) {
         displayMessage(`You Got it Right! The Number was ${randomNumber}`); 
         endGame();
     }else if (userinput > randomNumber) {
-        displayMessage(`Your Guess is too Higher than ${randomNumber}`);
+        displayMessage(`Your Guess is too Higher than`);
         // lowOrHi.textContent = 'Your Guess is Higher than the Number';
     }else if (userinput < randomNumber) {
-        displayMessage(`Your Guess is too Lower than ${randomNumber}`);
+        displayMessage(`Your Guess is too Lower than`);
         // lowOrHi.textContent = 'Your Guess is Lower than the Number';
     }
 
@@ -71,8 +73,11 @@ function displayGuesses(guess){
     // it wipes all the previous guess from guessSlot and display the new guess
     userguess.value = '';
     guessSlot.innerHTML += `${guess}, `;
+
     guessCountNumber++;
-    guessCount.innerHTML = `${11 - guessCountNumber}`;
+   const guesscount = guessCount.innerHTML = `${11 - guessCountNumber}`;
+    // console.log(guesscount);    //to test the guess count number
+    
 };
 
 function displayMessage(message){
@@ -81,12 +86,32 @@ function displayMessage(message){
     
 };
 
-function newGame(){
-    //
+function endGame(){
+    // End the game when the user is wrong or correct
+    userguess.value = '';
+    userguess.setAttribute('disabled','');
+    playGame = false;
+    p.classList.add('button');
+    p.innerHTML = `<h2 id="newGame"> Start again </h2>`;
+    startOver.appendChild(p);
+    newGame();
 };
 
-function endGame(){
-    //
+function newGame(){
+    // when the user press the new game button the game start again
+    const startNewGame = document.querySelector("#newGame");
+    startNewGame.addEventListener('click', function(e){
+       
+        randomNumber = parseInt(Math.random() * 100 + 1);
+        previousGuess = [];
+        guessCountNumber = 1;
+        guessSlot.innerHTML = '';
+        guessCount.innerHTML = `${11-guessCountNumber}`;
+        userguess.removeAttribute('disabled');
+        startOver.removeChild(p);
+        playGame = true;
+    });
 };
+
 
 
